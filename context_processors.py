6d4ba53287b5ded9ee98d19445f7460ddb7a5b03
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-02-20 00:40:13 +0100

    working version

--- context_processors.py
+++ context_processors.py
@@ -5,5 +5,5 @@ from django.template.loader import render_to_string
 
 def google_analytics(request):
     if settings.DEBUG:
-        return {'google_analytics': ""}
-    return {'google_analytics': render_to_string("google_analytics.html", {'ga_key': settings.GOOGLE_ANALYTICS_KEY, 'ga_site': settings.GOOGLE_ANALYTICS_SITE})}
+        return {'google_analytics': "<!-- temp Google Analytics -->"}
+    return {'google_analytics': render_to_string("google/google_analytics.html", {'ga_key': settings.GOOGLE_ANALYTICS_KEY, 'ga_site': settings.GOOGLE_ANALYTICS_SITE})}
