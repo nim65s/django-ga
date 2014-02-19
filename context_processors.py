a927103155ac03311160943accfc13d36d59f965
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-02-20 00:01:10 +0100

    avec le code…

---
+++ context_processors.py
@@ -0,0 +1,9 @@
+#-*- coding: utf-8 -*-
+from django.conf import settings
+from django.template.loader import render_to_string
+
+
+def google_analytics(request):
+    if settings.DEBUG:
+        return {'google_analytics': ""}
+    return {'google_analytics': render_to_string("google_analytics.html", {'ga_key': settings.GOOGLE_ANALYTICS_KEY, 'ga_site': settings.GOOGLE_ANALYTICS_SITE})}
