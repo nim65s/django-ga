92ca1afd9f102c4e652d7f70668aa73295729675
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-12-26 11:54:41 +0100

    mark_safe

--- context_processors.py
+++ context_processors.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
 
 
 def google_analytics(request):
     if settings.DEBUG:
-        return {'google_analytics': "<!-- temp Google Analytics -->"}
+        return {'google_analytics': mark_safe("<!-- temp Google Analytics -->")}
     return {'google_analytics': render_to_string("google/google_analytics.html",
         {'ga_key': settings.GOOGLE_ANALYTICS_KEY, 'ga_site': settings.GOOGLE_ANALYTICS_SITE})}
